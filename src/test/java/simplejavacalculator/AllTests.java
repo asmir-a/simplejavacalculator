1a5e5aae68d7fffcf17d19d5ecece3d2fa92ac30
Adil Botabekov <botabekov.adil@gmail.com>
2020-06-13 23:25:19 +0600

    starter over (working)

---
+++ src/test/java/simplejavacalculator/AllTests.java
@@ -0,0 +1,14 @@
+package simplejavacalculator;
+
+import org.junit.Assert;
+import org.junit.Assert.*;
+import org.junit.Test;
+
+public class AllTests {
+
+    @Test
+    public void sampleTest() {
+        Assert.assertEquals(1,1);
+    }
+
+}
